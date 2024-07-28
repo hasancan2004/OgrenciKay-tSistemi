2a20bcf2a685d0644f994e9889765db24612758f
Hasan Can KULA <hasancan.kula0707@gmail.com>
2024-07-28 21:40:15 +0300

    The project is created

---
+++ Ogrenci_Kayit_Sistemi/FrmOgrenciDetay.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Ogrenci_Kayit_Sistemi
+{
+    partial class FrmOgrenciDetay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumara = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSinav1 = new System.Windows.Forms.Label();
+            this.lblSinav3 = new System.Windows.Forms.Label();
+            this.lblSinav2 = new System.Windows.Forms.Label();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNumara
+            // 
+            this.lblNumara.AutoSize = true;
+            this.lblNumara.Location = new System.Drawing.Point(176, 94);
+            this.lblNumara.Name = "lblNumara";
+            this.lblNumara.Size = new System.Drawing.Size(56, 25);
+            this.lblNumara.TabIndex = 0;
+            this.lblNumara.Text = "0000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sınav 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Sınav 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Sınav 3:";
+            // 
+            // lblSinav1
+            // 
+            this.lblSinav1.AutoSize = true;
+            this.lblSinav1.Location = new System.Drawing.Point(176, 181);
+            this.lblSinav1.Name = "lblSinav1";
+            this.lblSinav1.Size = new System.Drawing.Size(34, 25);
+            this.lblSinav1.TabIndex = 0;
+            this.lblSinav1.Text = "00";
+            // 
+            // lblSinav3
+            // 
+            this.lblSinav3.AutoSize = true;
+            this.lblSinav3.Location = new System.Drawing.Point(176, 261);
+            this.lblSinav3.Name = "lblSinav3";
+            this.lblSinav3.Size = new System.Drawing.Size(34, 25);
+            this.lblSinav3.TabIndex = 0;
+            this.lblSinav3.Text = "00";
+            this.lblSinav3.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // lblSinav2
+            // 
+            this.lblSinav2.AutoSize = true;
+            this.lblSinav2.Location = new System.Drawing.Point(176, 222);
+            this.lblSinav2.Name = "lblSinav2";
+            this.lblSinav2.Size = new System.Drawing.Size(34, 25);
+            this.lblSinav2.TabIndex = 0;
+            this.lblSinav2.Text = "00";
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(176, 40);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(62, 25);
+            this.lblAdSoyad.TabIndex = 0;
+            this.lblAdSoyad.Text = "NULL";
+            this.lblAdSoyad.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(60, 94);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(87, 25);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Numara:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(60, 306);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(98, 25);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Ortalama:";
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Location = new System.Drawing.Point(176, 306);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(34, 25);
+            this.lblOrtalama.TabIndex = 0;
+            this.lblOrtalama.Text = "00";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(60, 349);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(76, 25);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "Durum:";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(176, 349);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(62, 25);
+            this.lblDurum.TabIndex = 0;
+            this.lblDurum.Text = "NULL";
+            // 
+            // FrmOgrenciDetay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(803, 429);
+            this.Controls.Add(this.lblSinav3);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblSinav1);
+            this.Controls.Add(this.lblSinav2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblNumara);
+            this.Controls.Add(this.lblAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmOgrenciDetay";
+            this.Text = "Öğrenci Not Kayıt Sistemi";
+            this.Load += new System.EventHandler(this.FrmOgrenciDetay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNumara;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSinav1;
+        private System.Windows.Forms.Label lblSinav3;
+        private System.Windows.Forms.Label lblSinav2;
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
---
+++ Ogrenci_Kayit_Sistemi/FrmOgrenciDetay.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ogrenci_Kayit_Sistemi
+{
+    public partial class FrmOgrenciDetay : Form
+    {
+        public FrmOgrenciDetay()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        public string numara;
+        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-JNFOI6TN\SQLHASAN;Initial Catalog=DbNotKayit;Integrated Security=True;Encrypt=False");
+        
+        private void FrmOgrenciDetay_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("SELECT * From TBLDERS where OGRNUMARA = @p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", numara);
+            lblNumara.Text = numara;
+            SqlDataReader dr = komut.ExecuteReader();
+            while(dr.Read())
+            {
+                lblAdSoyad.Text = dr[2].ToString() + " " + dr[3].ToString();    lblSinav1.Text = dr[4].ToString();
+                lblSinav2.Text = dr[5].ToString();
+                lblSinav3.Text = dr[6].ToString();
+                lblOrtalama.Text = dr[7].ToString();
+                lblDurum.Text = dr[8].ToString();
+            }
+             if(lblDurum.Text == "True")
+             {
+                lblDurum.Text = "Geçti";
+             }
+             else
+             {
+                lblDurum.Text = "Kaldı";
+             }
+           
+        }
+    }
+}
